272e8ae2d6730371c69387ca12a225411906522d
Isaac Charco <i.charco@correo.unimet.edu.ve>
2026-06-06 20:46:49 -0400

    Add files via upload

---
+++ ProyectoNeuronal/src/Clases/GestorFatiga.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+import Edd.Nodo;
+        
+/**
+ *
+ * @author ischl
+ */
+public class GestorFatiga {
+
+    /**
+     * Aplica fatiga a toda la red neuronal: cada conexión ve su coeficiente k multiplicado por 1.2.
+     *
+     * @param red Red neuronal cuyas conexiones se modificarán.
+     */
+    public void aplicarFatiga(redNeuronal red) {
+        Nodo<ConexionSinaptica> actual = red.getTodasLasConexiones().getCabeza();
+        while (actual != null) {
+            double nuevoK = actual.getDato().getCoeficienteEficiencia() * 1.2;
+            actual.getDato().setCoeficienteEficiencia(nuevoK);
+            actual = actual.getSiguiente();
+        }
+    }
+}
+
