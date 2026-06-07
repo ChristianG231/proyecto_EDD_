088c5ac1ce4a10c591387f836aea0e189d3286f9
Isaac Charco <i.charco@correo.unimet.edu.ve>
2026-06-06 20:27:31 -0400

    Update NodoHash.java

--- ProyectoNeuronal/src/Edd/NodoHash.java
+++ ProyectoNeuronal/src/Edd/NodoHash.java
@@ -1,35 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Edd;
 
 /**
  * Nodo para la lista enlazada interna de la tabla hash.
- * Cada nodo guarda un ParClaveValor y apunta al siguiente.
- * Se usa cuando dos claves caen en un mismo indice, cuando colisionan.
+ * Guarda un {@link ParClaveValor} y una referencia al siguiente nodo.
+ *
+ * @param <K> Tipo de la clave.
+ * @param <V> Tipo del valor.
  * @author ischl
- * @param <K>
- * @param <V>
  */
-public class NodoHash<K, V>{
+public class NodoHash<K, V> {
     private final ParClaveValor<K, V> dato;
     private NodoHash<K, V> siguiente;
-    
-    public NodoHash(ParClaveValor<K, V> dato){
+
+    /**
+     * Constructor.
+     *
+     * @param dato Par clave-valor que almacena este nodo.
+     */
+    public NodoHash(ParClaveValor<K, V> dato) {
         this.dato = dato;
         this.siguiente = null;
     }
-    
-    public ParClaveValor<K, V> getDato(){
+
+    /**
+     * @return El dato almacenado.
+     */
+    public ParClaveValor<K, V> getDato() {
         return dato;
     }
-    
+
+    /**
+     * @return El siguiente nodo en la lista.
+     */
     public NodoHash<K, V> getSiguiente() {
         return siguiente;
     }
-    
-    public void setSiguiente(NodoHash<K, V> siguiente){
+
+    /**
+     * Asigna el siguiente nodo.
+     *
+     * @param siguiente Nodo a enlazar.
+     */
+    public void setSiguiente(NodoHash<K, V> siguiente) {
         this.siguiente = siguiente;
     }
 }
